f8657503edb9ed437ab75953ad5659a6fcac6f24
eliguy343 <eliguy343@gmail.com>
2026-01-27 23:53:10 +0200

    redux store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,4 @@
-import { UserData } from "@/types/userTypes";
+import { UserData, UserDataPayload } from "@/types/userTypes";
 import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { useDispatch, useSelector, useStore } from "react-redux";
 
@@ -13,7 +13,38 @@ const userDataSlice = createSlice({
   name:"user",
   initialState: initialUserData,
   reducers: {
-    
+    loginUser(state, action: PayloadAction<UserDataPayload>) {
+      console.log(`payload: ${action.payload}`);
+      state = {...action.payload, loggedIn: true};
+      return state;
+    },
+    logoutUser(state) {
+      state = {
+        loggedIn: false,
+        username: "",
+        email:"",
+        avatar_url: ""
+      }
+      return state;
+    }
   }
 });
 
+
+export const makeStore = () => {
+  return configureStore({
+    reducer:{
+      userData: userDataSlice.reducer,
+      
+    }
+  })
+}
+
+export const {loginUser, logoutUser} = userDataSlice.actions;
+
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
