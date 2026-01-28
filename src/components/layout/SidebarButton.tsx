3309d1e3870d6bbf39ddd7d9761c856bc68f82fc
eliguy343 <eliguy343@gmail.com>
2026-01-28 22:15:14 +0200

    sidebar design

--- src/components/layout/SidebarButton.tsx
+++ src/components/layout/SidebarButton.tsx
@@ -12,13 +12,17 @@ const SidebarButton = ({linkTo, IconComp, btnTitle} : sbtnProps) => {
   return (
     <Link 
       href={linkTo} 
-      className='flex items-center p-3 justify-between border-2 
+      className='flex flex-col items-center p-3  gap-2 justify-between  
         border-gray-600 rounded-2xl w-[80%] hover:bg-gray-200 transition-all duration-300'
     >
-      <IconComp color='var(--color-gray-800)' className='w-10' size={20}/>
-      <p className='text-xl font-bold text-black'>
-        {btnTitle}
-      </p>
+      <div className='flex items-center justify-between w-full'>
+        <IconComp color='var(--color-gray-800)' className='w-10' size={20}/>
+        <p className='text-xl font-bold text-black'>
+          {btnTitle}
+        </p>
+
+      </div>
+      <div className='w-full border border-dashed border-gray-600 rounded-4xl'></div>
     </Link>
   )
 }
