cd162969b86d804112362325ae9d6d3045014cd6
eliguy343 <eliguy343@gmail.com>
2026-01-31 22:54:58 +0200

    on submit register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,4 @@
+import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
@@ -6,18 +7,39 @@ type registerForm = {
   email: string,
   password: string
 }
+
+type ApiResponseError = {
+  msg: string
+}
+
 const page = () => {
   const {register, handleSubmit, formState, setError} = useForm<registerForm>();
   const [apiError, setApiError] = useState("");
   const router = useRouter();
-
+  const onSubmit = async (data: registerForm) => {
+    try {
+      const res = await axios.post(`/api/auth/login`, {
+       username: data.username,
+       email: data.email,
+       password: data.password 
+      })
+      router.push('/');
+    } catch (err) {
+      const axErr = err as AxiosError<ApiResponseError>;
+      console.log(axErr.response?.data?.msg);
+      if(axErr.response?.data?.msg) setApiError(axErr.response.data.msg)
+      setTimeout(() => {
+        setApiError("")
+      }, 3500);
+    }
+  }
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center bg-slate-200'>
     <div 
         className='w-[70%] md:w-[50%] lg:w-[30%] h-[80%] lg:h-[50%] shadow-xl border border-slate-200 bg-white
           rounded-xl flex flex-col justify-center p-5 overflow-scroll'
       >
-        
+
       </div>
     </div>
   )
