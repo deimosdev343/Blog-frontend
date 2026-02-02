77dd979baf5f90c310d2a93b7afdcd66bd110b40
eliguy343 <eliguy343@gmail.com>
2026-02-02 20:52:44 +0200

    recommendSearchBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import RecommendSearchBar from "@/components/layout/RecommendSearchBar";
 import SideBar from "@/components/layout/SideBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-slate-700 w-full h-screen">
+    <div className="flex bg-slate-700 w-full h-screen">
       <SideBar/>
+      <div className="w-[64%] h-screen">
+      </div>
+      <RecommendSearchBar/>
     </div>
   );
 }
---
+++ src/components/layout/RecommendSearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecommendSearchBar = () => {
+  return (
+        <div className='h-full w-[22%] hidden md:flex flex-col p-2 gap-2 items-center border-r-2 bg-gray-100 border-slate-200'>
+          RecommendSearchBar
+        </div>
+  )
+}
+
+export default RecommendSearchBar
