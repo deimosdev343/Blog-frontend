20e9fc0a70ee0c094c6a36dff188904d3aadc99c
eliguy343 <eliguy343@gmail.com>
2026-02-03 23:07:03 +0200

    styling

--- src/components/layout/RecommendSearchBar/RecommendSearchBar.tsx
+++ src/components/layout/RecommendSearchBar/RecommendSearchBar.tsx
@@ -9,7 +9,7 @@ const RecommendSearchBar = () => {
     searchTerm:""
   })
   return (
-    <div className='h-full w-[22%] hidden md:flex flex-col p-2 gap-2 items-center border-l-2 bg-gray-100 border-slate-200'>
+    <div className='h-full w-[30%] hidden md:flex flex-col p-2 gap-2 items-center border-l-2 bg-gray-100 border-slate-200'>
       <SearchBar 
         Icon={FaSearch}
         onChange={(text) => {
