800886aaadaccd99bf09b2cca186c68d620a161a
eliguy343 <eliguy343@gmail.com>
2026-02-05 21:40:15 +0200

    tiptap

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -1,8 +1,11 @@
+import Tiptap from '@/components/Editor/Tiptap'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-[60%]'>page</div>
+    <div className='w-[60%] h-screen'>
+      <Tiptap/>
+    </div>
   )
 }
 
