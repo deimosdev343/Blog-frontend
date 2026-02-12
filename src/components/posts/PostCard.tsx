ee9eb9fa5b3abc36a4569486c68eda8bf3ab28f3
eliguy343 <eliguy343@gmail.com>
2026-02-12 22:40:45 +0200

    Post Card

---
+++ src/components/posts/PostCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type PostCardProps = {
+  id: string
+  title: string
+  previewText: string
+  avatarUrl: string
+}
+
+const PostCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostCard
--- src/components/posts/PostComponent.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PostComponent = () => {
-  return (
-    <div>PostComponent</div>
-  )
-}
-
-export default PostComponent
