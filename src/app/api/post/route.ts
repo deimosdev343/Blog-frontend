0071289c7433304ab53757c63286d4683b1581a6
eliguy343 <eliguy343@gmail.com>
2026-02-12 20:15:49 +0200

    get posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -5,6 +5,34 @@ type ApiResponseError = {
   msg: string
 }
 
+export const GET = async (req: NextRequest) => { 
+  try {
+    const cks = await cookies();
+    const token =  cks.get("token")?.value;
+
+    const backendRes = await axios.get(
+      `${process.env.BACKEND_API}/posts`,
+      {
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      }
+    );
+    return NextResponse.json(
+      backendRes.data,
+      {status: 200}
+    );
+  } catch (err) {
+    console.log(err);
+    const axErr = err as AxiosError<ApiResponseError>;
+    console.log(axErr.response);
+    return NextResponse.json(
+      {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
+      {status:axErr.response?.status || 500}
+    );
+  }
+}
+
 export const POST = async (req: NextRequest) => {
   try {
     const cks = await cookies();
