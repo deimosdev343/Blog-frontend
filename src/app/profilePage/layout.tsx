a8073c80eb5ab1ad996309bd6c6da6d97659850d
eliguy343 <eliguy343@gmail.com>
2026-02-13 22:32:17 +0200

    profile page

---
+++ src/app/profilePage/layout.tsx
@@ -0,0 +1,17 @@
+import RecommendSearchBar from '@/components/layout/RecommendSearchBar/RecommendSearchBar'
+import SideBar from '@/components/layout/SideBar/SideBar'
+import React from 'react'
+
+const layout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div className='w-full h-screen flex'>
+      <SideBar/>
+      <div className="w-[64%] h-screen">
+        {children}
+      </div>
+      <RecommendSearchBar/>
+    </div>
+  )
+}
+
+export default layout
---
+++ src/app/profilePage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen flex flex-col'>
+      
+    </div>
+  )
+}
+
+export default page
