40933ec51da250723da84d1ed715caddba8179bb
eliguy343 <eliguy343@gmail.com>
2026-02-15 22:56:13 +0200

    Post list pagination

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -1,20 +1,43 @@
 "use client";
 
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PostCard from './PostCard';
 import {Post} from '../../types/postTypes'
+
+const LIMIT = 10;
+
+
 const PostList = () => {
-  const fetchPosts = async () => {
-    const res = await axios.get('/api/post');
-    setPosts(res.data);
-  }
-  
+  const [PostFetchState, setPostFetchState] = useState({
+    skip: 0,
+    loading: false,
+    hasMore: true
+  })
   const [posts, setPosts] = useState<Post[]>([]);
-  console.log(posts);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const fetchPosts = async () => {
+    if (PostFetchState.loading || !PostFetchState.hasMore) return;
+    setPostFetchState(st => ({...st, loading: true}));
+    const res = await axios.get(`/api/post?skip=${PostFetchState.skip}&limit=${LIMIT}`);
+    const newPosts = res.data;
+    if (newPosts.length < LIMIT) {
+      setPostFetchState(st => ({...st, hasMore: false}));
+    }
+    setPosts(prev => [...prev, ...newPosts]);
+    setPostFetchState(st => ({...st, skip: st.skip + LIMIT, loading: false}));
+  };
+
+
+  // const fetchPosts = async () => {
+  //   const res = await axios.get('/api/post');
+  //   setPosts(res.data);
+  // }  
+
   useEffect(() => {
     fetchPosts();
-  }, [])
+  }, []);
+
   return (
     <div className='w-full h-full flex flex-col items-center gap-2 overflow-scroll py-2'>
       {posts.map(pst => <PostCard 
