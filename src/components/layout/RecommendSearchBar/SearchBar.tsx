d9635a5cbf4594b0528f2e928b16bc64310fbdd5
eliguy343 <eliguy343@gmail.com>
2026-02-20 13:49:32 +0200

    SearchBar

--- src/components/layout/RecommendSearchBar/SearchBar.tsx
+++ src/components/layout/RecommendSearchBar/SearchBar.tsx
@@ -10,17 +10,17 @@ interface SearchInputProps {
 
 const SearchBar = ({Icon, value, placeholder, onChange} :SearchInputProps) => {
   return (
-     <div className="relative w-full max-w-sm">
-      <div className="absolute inset-y-0 left-2 flex items-center text-gray-400 cursor-pointer">
-        <Icon size={25} color='#777'/>
+    <div className="relative w-full max-w-sm">
+      <div className="absolute inset-y-0 left-2 flex items-center text-[#2f54a5] cursor-pointer">
+        <Icon color={"#2f54a5"} size={20} className="transition-all duration-300 z-10" />
       </div>
       <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
-        className="w-full rounded-md border-2 border-dotted border-gray-500 py-2 pl-9 pr-3 
-          text-lg font-bold placeholder-gray-400 focus:outline-none"
+        className="w-full rounded-2xl bg-white/70 backdrop-blur-sm border border-gray-200 shadow-sm py-2 pl-9 pr-3
+          text-lg font-semibold placeholder-gray-400 focus:outline-none focus:bg-indigo-50 transition-all duration-300"
       />
     </div>
   )
