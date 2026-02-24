6481215146c7227de28d997b71fa69b947f6f30e
eliguy343 <eliguy343@gmail.com>
2026-02-24 21:21:25 +0200

    follow user

---
+++ src/app/api/follow/route.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+type ApiResponseError = {
+  msg: string
+}
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const {user_id} = await req.json();
+    const cks = await cookies();
+    const token =  cks.get("token")?.value;
+
+    const res = await axios.post(`/${process.env.BACKEND_API}/user/follow`, 
+      {
+        follow_user_id: user_id
+      },
+      {
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      }
+    )
+    return NextResponse.json(
+      res.data,
+      {status:200}
+    );
+    
+  } catch (err) {
+      console.log(err);
+      const axErr = err as AxiosError<ApiResponseError>;
+      console.log(axErr.response);
+      return NextResponse.json(
+        {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
+        {status:axErr.response?.status || 500}
+      );
+  }
+}
