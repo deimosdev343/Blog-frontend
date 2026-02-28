b920df5dd057152d219baee621c382091a5d658b
Eli Guy <eliguy343@gmail.com>
2026-02-28 16:49:22 +0200

    unfollow api call

--- src/app/api/follow/route.ts
+++ src/app/api/follow/route.ts
@@ -38,3 +38,30 @@ export const POST = async (req: NextRequest) => {
   }
 }
 
+
+export const DELETE = async (req: NextRequest) => {
+  try {
+    const {user_id} = await req.json();
+    const cks = await cookies();
+    const token =  cks.get("token")?.value;
+    const res = await axios.delete(`/${process.env.BACKEND_API}/user/follow/${user_id}`, 
+      {
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      }
+    )
+    return NextResponse.json(
+      res.data,
+      {status:200}
+    );
+  } catch (err) {
+    console.log(err);
+    const axErr = err as AxiosError<ApiResponseError>;
+    console.log(axErr.response);
+    return NextResponse.json(
+      {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
+      {status:axErr.response?.status || 500}
+    );
+  }
+}
