8ae14b65bdc9c50297796a2d4de587b3e4b7896f
eliguy343 <eliguy343@gmail.com>
2026-03-12 22:52:11 +0200

    get post votes

---
+++ src/app/api/vote/route.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+type ApiResponseError = {
+  msg: string
+}
+
+
+export const GET = async (req: NextRequest) => {
+  try {
+    const {searchParams} = new URL(req.url);
+    const cks = await cookies();
+    const token =  cks.get("token")?.value;
+    const post_id = searchParams.get("post_id");
+
+    if(!post_id) {
+      return NextResponse.json(
+        {msg:"post_id needs to be included"},
+        {status:401}
+      )
+    }
+    const backendRes = await axios.get(
+      `${process.env.BACKEND_API}/vote/${post_id}`,
+      {
+        headers:{
+          Authorization: `bearer ${token}`
+        }
+      }
+    );
+    return NextResponse.json(
+      backendRes.data,
+      {status:200}
+    )
+  } catch (err) {
+    console.log(err);
+    const axErr = err as AxiosError<ApiResponseError>;
+    console.log(axErr.response);
+    return NextResponse.json(
+      {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
+      {status:axErr.response?.status || 500}
+    );
+  }
+}
