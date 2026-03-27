25ecec8a419678412e149c630f86f5570d9864c4
Eli Guy <eliguy343@gmail.com>
2026-03-27 19:44:26 +0300

    get comments of post api

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -5,23 +5,20 @@ type ApiResponseError = {
   msg: string
 }
 
-export const GET = async (req: NextRequest) => { 
+export const GET = async (req: NextRequest) => {
   try {
     const {searchParams} = new URL(req.url);
+    const commentId = searchParams.get("post_id");
+
     const skip = parseInt(searchParams.get("skip") ?? "0");
     const limit = parseInt(searchParams.get("limit") ?? "10");
-    const cks = await cookies();
-    const token =  cks.get("token")?.value;
     const backendRes = await axios.get(
-      `${process.env.BACKEND_API}/posts/following/feed`,
+      `${process.env.BACKEND_API}/comment/${commentId}`,
       {
         params:{
           skip,
           limit
         },
-        headers:{
-          Authorization: `bearer ${token}`
-        }
       }
     );
     return NextResponse.json(
@@ -36,5 +33,5 @@ export const GET = async (req: NextRequest) => {
       {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
       {status:axErr.response?.status || 500}
     );
-  }
+  } 
 }
