439242cb05eb043eaa3d5a67cbb5347879431a7e
eliguy343 <eliguy343@gmail.com>
2026-04-01 23:38:39 +0300

    fetch comments call

--- src/components/comments/CommentBox.tsx
+++ src/components/comments/CommentBox.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 type CommentBoxProps = {
@@ -9,10 +10,25 @@ type CommentBoxProps = {
 const CommentBox = ({post_id} : CommentBoxProps) => {
   const [comments, setComments] = useState([]);
 
+  const fetchComments = async () => {
+    try {
+      const res = await axios.get("/api/comment", {
+        params:{
+          post_id,
+          skip: 0,
+          limit: 10
+        }
+      })
+      console.log(res);
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
   useEffect(() => {
-
+    fetchComments();
   }, []);
-
+  
   return (
     <div className='w-full flex flex-col'>
       
