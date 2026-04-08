a244c8701225c10a15aa160a871b019e9e12d81b
eliguy343 <eliguy343@gmail.com>
2026-04-08 23:33:12 +0300

    text suggestions api

---
+++ src/app/api/ai/suggestions/route.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosError } from "axios";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+type ApiResponseError = {
+  msg: string
+}
+
+export const POST = async (req: NextRequest) => {
+  try {
+    // const cks = await cookies();
+    // const token =  cks.get("token")?.value;
+    const body = await req.json();
+
+    const backendRes = await axios.post(
+      `${process.env.BACKEND_API}/ai/suggests`,
+      body,
+      {
+      }
+    );
+    return NextResponse.json(
+      backendRes.data,
+      {status: 200}
+    );
+  } catch (err) {
+    console.log(err);
+    const axErr = err as AxiosError<ApiResponseError>;
+    console.log(axErr.response);
+    return NextResponse.json(
+      {msg:axErr.response?.data?.msg || "Internal Server Error"}, 
+      {status:axErr.response?.status || 500}
+    );
+  }
+}
