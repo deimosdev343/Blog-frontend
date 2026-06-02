3d3576fa6700a7e22c822f16ec097e96ec74153e
eliguy343 <eliguy343@gmail.com>
2026-06-02 07:28:16 +0300

    fixed jest import

--- src/app/post/[slug]/page.test.tsx
+++ src/app/post/[slug]/page.test.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import Page from './page';
-import {describe, expect, it, jest, test} from '@jest/globals';
+import {describe, expect, it, test} from '@jest/globals';
+import axios from 'axios';
+
 
 jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 it('renders page', async () => {
+  mockedAxios.get.mockResolvedValue({
+    data: {
+      title: "Test",
+      content: "test"
+    }
+  });
   const page = await Page({
     params: {
       slug: "12",
