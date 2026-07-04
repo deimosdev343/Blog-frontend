1b70292b45c0fcabf143cc0a9c6135713278290d
Eli Guy <eliguy343@gmail.com>
2026-07-04 15:27:57 +0300

    comment type fix

--- src/types/commentTypes.ts
+++ src/types/commentTypes.ts
@@ -1,3 +1,9 @@
+export interface Author {
+  id: number,
+  username: string,
+  avatar_url: string
+}
+
 export interface Comment {
   id: number,
   author_id: number,
@@ -6,4 +12,6 @@ export interface Comment {
   user_avatar?: string
   created_at?: Date,
   updated_at?: Date,
+  author: Author
+  
 }
